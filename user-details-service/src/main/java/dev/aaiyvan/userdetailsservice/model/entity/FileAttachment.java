80020e2acef74da3f11f815c3d08808f7a179116
KvaAan <korzhenevskiy05@yandex.ru>
2024-07-09 22:56:27 +0400

    add FileAttachment

---
+++ user-details-service/src/main/java/dev/aaiyvan/userdetailsservice/model/entity/FileAttachment.java
@@ -0,0 +1,33 @@
+package dev.aaiyvan.userdetailsservice.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "file_attachments")
+public class FileAttachment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(name = "file_name", nullable = false)
+    private String fileName;
+
+    @Column(nullable = false)
+    private String source;
+
+    @Column(nullable = false)
+    private String url;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_info_id", nullable = false)
+    private UserInfo userInfo;
+}
